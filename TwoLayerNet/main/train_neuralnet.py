1faf45e5166ef915198462bf79f86a9416c47777
しゅうへい <yamepi999sato@gmail.com>
2022-09-22 13:10:36 +0900

    two_layer_netのモジュールインポート部分を訂正

--- TwoLayerNet/main/train_neuralnet.py
+++ TwoLayerNet/main/train_neuralnet.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)
+from os.path import dirname, abspath
+sys.path.append(dirname(dirname(abspath(__file__)))) 
 
 import numpy as np
 from dataset.mnist import load_mnist
-from two_layer_net import TwoLayerNet
+from main.two_layer_net import TwoLayerNet
 
 # データの読み込み
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
