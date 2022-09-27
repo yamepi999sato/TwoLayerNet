f45c3180a9ac7f7723a8b662827d43240abffa13
しゅうへい <yamepi999sato@gmail.com>
2022-09-27 21:19:54 +0900

    誤差グラフ表示 収束してなさそう

--- TwoLayerNet/main/train_neuralnet.py
+++ TwoLayerNet/main/train_neuralnet.py
@@ -34,14 +34,14 @@ def p(x, N):
     return ( wave_func(x, N) )**2
 
 
-i = 10000
-M = int(i/10)
+M = 1000
+i = int(M*10)
 sdata= np.empty((M+1, N))
 
 x_train = np.empty((M, N))
 x_test = np.empty((M, N))
 
-x = np.zeros(N)
+x = np.empty(N)
 cnt=0
 
 for _ in range(i):
@@ -53,7 +53,7 @@ for _ in range(i):
     x = y
     cnt += 1
     if cnt%10==0:
-        x_train[int(cnt/10)-1] = x
+        x_train[M-1] = x
 
 cnt = 0
 x = np.zeros(N)
@@ -66,7 +66,7 @@ for _ in range(i):
     x = y
     cnt += 1
     if cnt%10==0:
-        x_test[int(cnt/10)-1]= x
+        x_test[M-1]= x
 
 t_train = wave_func(x_train, N).reshape(-1, 1)
 t_test = wave_func(x_test, N).reshape(-1, 1)
@@ -102,7 +102,7 @@ plt.show()
 """
 network = TwoLayerNet(input_size=N, hidden_size=5, output_size=1)
 
-iters_num = 10000
+iters_num = 100000
 train_size = x_train.shape[0]
 batch_size = 5
 learning_rate = 0.0001
@@ -110,8 +110,7 @@ learning_rate = 0.0001
 
 
 train_loss_list = []
-train_acc_list = []
-test_acc_list = []
+
 
 train_err_list =[]
 train_err_list = []
@@ -120,19 +119,7 @@ train_err_list = []
 test_err_list = []
 
 iter_per_epoch = max(train_size / batch_size, 1)
-"""
-#条件を表示
-print("input_size(入力層ユニット数) = N(粒子数): " + str(N) + "(個)")
-print("hidden_size(隠れ層ユニット数): " + str(network.hidden_size) + "個")
-print("output_size(出力層ユニット数): " + str(network.output_size) + "個")
-print("train_size(全サンプル数): " + str(train_size) + "個")
-print("batch_size(バッチサイズ): " + str(batch_size) + "個")
-print("iter_per_epoch(1エポックの更新回数): " + str(iter_per_epoch) + "回")
-print("iters_num(全更新回数): " + str(iters_num) + "回")
-print("learning_rate(学習率): " + str(learning_rate))
-print("\n")
-print("train_err(誤差)")
-"""
+
 
 #条件を表示
 condition = \
@@ -157,7 +144,6 @@ for i in range(iters_num):
     #print(t_batch.shape)                                    # (batch_size, 1)
     
     # 勾配
-    #grad = network.numerical_gradient(x_batch, t_batch)
     grad = network.gradient(x_batch, t_batch)
     
     # 更新
@@ -170,10 +156,6 @@ for i in range(iters_num):
     if i % iter_per_epoch == 0:
     #if 1:
         j = int(i/ iter_per_epoch)
-        train_acc = network.accuracy(x_train, t_train)
-        test_acc = network.accuracy(x_test, t_test)
-        train_acc_list.append(train_acc)
-        test_acc_list.append(test_acc)
         
         
         train_err = network.error(x_batch, t_batch)
@@ -189,11 +171,24 @@ for i in range(iters_num):
         #print((train_y-t_batch)/t_batch)                        # 正しい誤差の配列が表示される
         #print("err: " + str(np.mean(train_y-t_batch)/t_batch))  # 正しい誤差の平均が表示されない
         
-        print("i=" + str(i) + ": " + str(train_err))
+        #print("i=" + str(i) + ": " + str(train_err))
         #print(train_err)                                        # 正しい誤差の平均が表示される
 
 x_array = np.arange(0, iters_num, iter_per_epoch)
-        
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+fig.subplots_adjust(right=0.5)
+ax.set_title("error (y-t)/t", fontname="MS Gothic")             # タイトル
+ax.set_xlabel('iter_index i')                                   # x軸ラベル  
+ax.set_ylabel('error (y-t)/t')                                  # y軸ラベル
+ax.text(1.1, 0.5, condition, ha='left', va='center', transform=ax.transAxes, fontname="MS Gothic")   #表示するテキスト
+
+ax.plot(x_array, train_err_list)                                # x軸,y軸に入れるリスト
+plt.show()
+
+"""
 plt.title("error (y-t)/t", fontname="MS Gothic")
 plt.plot(x_array,train_err_list,color=(0.0,0.0,0.7))
 #plt.hist(x_train[:, 0], bins=100, density=True, color=(1.0,0,0.0))
@@ -204,3 +199,4 @@ plt.legend()
 #plt.ylim(-0.5, 2)
 plt.grid(True)
 plt.show()
+"""
